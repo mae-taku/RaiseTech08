414ba9dbb544afdcdfe5ebf15a5244bb41f7f0ae
maetaku <maetaku@localhost>
2022-01-14 09:25:56 +0900

    delete commit(5d027ef9c7cf1ca49bb016a05457aebbe1ad3e84)

--- RaiseTech08/src/main/java/com/example/demo/controller/TopController.java
+++ RaiseTech08/src/main/java/com/example/demo/controller/TopController.java
@@ -1,4 +1,8 @@
+<<<<<<< M01
 package com.example.demo.Controller;
+=======
+package com.example.demo.controller;
+>>>>>>> 5d027ef add MyBatis-Read with test method.
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
