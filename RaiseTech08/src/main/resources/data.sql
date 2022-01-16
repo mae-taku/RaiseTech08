77e83c14da00638608ee0e6c7b299a1a6590b840
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-01-16 20:16:23 +0900

    data.sql追加

---
+++ RaiseTech08/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO shopping_list(name, quantity, price) VALUES('りんご', 1, 100);
+INSERT INTO shopping_list(name, quantity, price) VALUES('バナナ', 1, 150);
+INSERT INTO shopping_list(name, quantity, price) VALUES('マンゴー', 1, 250);
+INSERT INTO shopping_list(name, quantity, price) VALUES('じゃがいも', 1, 100);
+INSERT INTO shopping_list(name, quantity, price) VALUES('玉ねぎ', 1, 30);
+INSERT INTO shopping_list(name, quantity, price) VALUES('ニンジン', 1, 100);
+INSERT INTO shopping_list(name, quantity, price) VALUES('牛肉', 1, 300);
+INSERT INTO shopping_list(name, quantity, price) VALUES('カレールー', 1, 200);
