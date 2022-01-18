2cb2afc66b93e540d317e70aa19badc89c867424
yochan0120 <yoko_whiterosa.alba@gamma.ocn.ne.jp>
2022-01-18 19:44:11 +0900

    data.sqlwo

--- RaiseTech08/src/main/resources/data.sql
+++ RaiseTech08/src/main/resources/data.sql
@@ -1,8 +1,9 @@
-INSERT INTO shopping_list(name, quantity, price) VALUES('りんご', 1, 100);
-INSERT INTO shopping_list(name, quantity, price) VALUES('バナナ', 1, 150);
-INSERT INTO shopping_list(name, quantity, price) VALUES('マンゴー', 1, 250);
-INSERT INTO shopping_list(name, quantity, price) VALUES('じゃがいも', 1, 100);
-INSERT INTO shopping_list(name, quantity, price) VALUES('玉ねぎ', 1, 30);
-INSERT INTO shopping_list(name, quantity, price) VALUES('ニンジン', 1, 100);
-INSERT INTO shopping_list(name, quantity, price) VALUES('牛肉', 1, 300);
-INSERT INTO shopping_list(name, quantity, price) VALUES('カレールー', 1, 200);
+INSERT INTO shopping_list (name, quantity, price) VALUES 
+ ('りんご', 1, 100),
+ ('バナナ', 1, 150),
+ ('マンゴー', 1, 250),
+ ('じゃがいも', 1, 100),
+ ('玉ねぎ', 1, 30),
+ ('ニンジン', 1, 100),
+ ('牛肉', 1, 300),
+ ('カレールー', 1, 200);
