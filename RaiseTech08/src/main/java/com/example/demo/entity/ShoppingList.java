a9ee5571c17c11b8710fd5d2ad716199387d9d2b
前田拓海 <maetaku@localhost>
2022-01-27 11:14:39 +0900

    change EntityをJpa仕様に変更。

--- RaiseTech08/src/main/java/com/example/demo/entity/ShoppingList.java
+++ RaiseTech08/src/main/java/com/example/demo/entity/ShoppingList.java
@@ -1,12 +1,18 @@
 package com.example.demo.entity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
+@Entity
 @Data
 public class ShoppingList {
 	
+	@Id
+	@GeneratedValue
 	private int id;
 	
 	@Max(50)
